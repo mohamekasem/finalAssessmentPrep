38ec02682b456e8bbf6bb9f823417698b51110d5
mohamed <toot2009@hotmail.com>
2016-12-21 12:56:43 +0200

    my ex

--- Part 1/part 1.js
+++ Part 1/part 1.js
@@ -1,38 +1,113 @@
-//=============================================================================
-/*                                  Part 1                                   */
-//=============================================================================
-/* (1) Think of some way to represent all of the students in RBK cohort2, if you know
-		that each one have the following attributs:
-		- name - age - education - nationality
+// //============================================================================
+// = /*                                  Part 1
+// */ //=========================================================================
+// ==== /* (1) Think of some way to represent all of the students in RBK cohort2,
+// if you know         that each one have the following attributs:         - name
+// 	- age - education - nationality         */ 
+function meke (n,a,e,na){
+	return {
+		name:n,  
+		age:a,      
+		education:e,
+		nationality:na    
+	} 
+}
 
-	a- represent 2 of your frineds in rbk using the model that you have made and name them as your frineds name
+    // a- represent 2 of your frineds in rbk using the model that you have
+    // made and name them as your frineds name 
+    var frind=[] 
+    var frind2=meke("zainab",22,"foodexpirians","syrian") 
+    var frind1=meke("ahmad",23,"comsaians","jordanian")
+    var frind3=meke("ruaa",22,"engelsh","phalstain") 
+    frind=[frind1,frind2,frind3]
 
-	b- create a function called showFriend() that accepts one parameter, and output representation of your frined like the following
-		showFriend(frinedData) // "Fatema with the age of 26 and with computer engineering education" 
-
-	c- create a function called avergeStudents() to calculate the average age of your class students, is it possible to pass all your class students one by one ? think of a way to pass them in your function
-*/
-// write your code here ...
-
-
-
-
-/*
- b-create a function called rangeSquared in which it will accept two parameters, and will output the squared numbers between these two parameter if the number is even 
-	in order to square the numbers create a function called square and call it inside rangeSquared function
-	rangeSquared(2)// [4];
-	rangeSquared(3) // null
-	rangeSquared(2,10)// [4,16,12,16,100];
-*/
-// write your code here ...
+    // b- create a function called showFriend() that accepts one parameter,
+// and output representation of your frined like the following     //
+// showFriend(frinedData) // "Fatema with the age of 26 and with computer
+// engineering education"  function showFriend(obj){     return obj.name+"
+// "+obj.age+" "+obj.education+" "+obj.nationality }     // c- create a function
+// called avergeStudents() to calculate the average age of your class students,
+// is it possible to pass all your class students one by one ? think of a way to
+// pass them in your function
 
 
+function avergeStudents(arr){  
+	var x=0   
+	for(var i=0;i<arr.length;i++){
+		x+=arr[i].age   
+	}
+	return x/arr.length 
+}
 
 
-/* c- Find all leaders in an array. A leader is an element larger than all elements to the right of it.
- 	Example:
-	leader([98, 20, 30, 5, 11, 27]) // output: [98, 30, 27]
-*/
 
-// write your code here ....
+
+
+//  // b-create a function called rangeSquared in which it will accept two
+// parameters, and will output the squared numbers between these two parameter if
+// the number is even      
+// in order to square the numbers create a function
+// called square and call     
+// it inside rangeSquared function
+// rangeSquared(2)= [4];
+ // rangeSquared(3)= null
+ // rangeSquared(2,10)=[4,16,36,64,100];
+ function squer (x,n){
+ var y=0;
+ if(n===undefined){
+ 	n=x
+ return Math.pow(x,n) 
+ }else{
+ 	return Math.pow(x,n)
+
+ }
+}
+function rangeSquared(x,n){
+	var arr=[]
+	var y=0
+	if(x%2===0){
+	if(n!==undefined){
+	for(var i = x ; i<=n ;i++){
+		
+			y+=squer(x)
+			arr.push(y)
+		}
+		
+	}else{
+		for(var i = 0 ; i<=x ;i++ ){
+
+		}
+	}
+	return arr
+}
+return null
+}
+	
+//???
+
+
+
+
+/* c- Find all leaders in an array. A leader is an element larger than all
+elements to the right of it.     Example:     leader([98, 20, 30, 5, 11, 27])
+// output: [98, 30, 27] */
+
+function leader(arr){
+	var x=arr[0]
+	arr=arr.splice(1);
+	arr1=[]
+	for(var i = 0 ; i < arr.length ; i++){
+    if(x>arr[i]){
+     arr.splice(arr[i])
+     arr1.push(x)
+     x=arr[i]
+   
+    }else{
+    	arr.splice(arr[i])
+    }
+	}
+	return arr1
+}
+
+
 
--- Part 2/Part 2.js
+++ Part 2/Part 2.js
@@ -1,34 +1,62 @@
 //=============================================================================
 /*                                  Part 2                                   */
 //=============================================================================
- /*
-
-  Hint: write the function in question (b, c, d) outside the Player scope 
-
-  Note : do not use (for, while, recursion) but you allowed to use the High order function
-    like (.map, .each, .filter, .reduce,.....) if you don't know how to use it just Google it 
  
- today we will create a football team using what we learn,
 
- 	a - we need to create the players using OOP the player declaration
-    should take the name of the player  	==> var player1 = Player("jony")
+ //  Hint: write the function in question (b, c, d) outside the Player scope 
 
-  b - after that we need function to add the other info for the player with addInfo function
-		==> player1.addInfo(age, position, level, availability );
 
-	c- and create another function to increase the the level for the player by n value 
-		==> player1.increaseLevel(4); 
+ //  Note : do not use (for, while, recursion) but you allowed to use the High order function
+ //    like (.map, .each, .filter, .reduce,.....) if you don't know how to use it just Google it 
+ 
+ // today we will create a football team using what we learn,
+function players(name){
 
-	d - we need another function to check if the player is available or not it should return true or false
-		==> player1.isAvailable();  true / false
+var obj={}
+obj.players=[]
+obj.name=name
+obj.addInfo=addInfo
+obj.increaseLevel=increaseLevel
+return obj
+}
+ 
 
-	e - then we need to create 4 players and add the info for them and put them inside the arrayOfPlayers
+ // 	a - we need to create the players using OOP the player declaration
+ //    should take the name of the player  	==> var player1 = Player("jony")
 
-	f - write function to iterate over the all players to decrease the level for the player if age larger the 30
+ //  b - after that we need function to add the other info for the player with addInfo function
+	// 	==> player1.addInfo(age, position, level, availability );
+function addinfo(age,po,lev,avl){
+return this.name{
+	age:age,
+	position:po,
+	lev:lev,
+	availability:avl
+}
+this.player.push(name)
+}
 
-  g - sort the players in the arrayOfPlayers by the key
-		sortPalyerBy(arrayOfPlayers, "age");
-		sortPalyerBy(arrayOfPlayers, "name");
- */
+	// c- and create another function to increase the the level for the player by n value 
+	// 	==> player1.increaseLevel(4); 
+function increaseLevel(n){
+	this.name.lev=n
+}
+	// d - we need another function to check if the player is available or not it should return true or false
+	// 	==> player1.isAvailable();  true / false
+function isAvailable(){
+if(this.name){
+	return true
+}
+return false
+}
+	// e - then we need to create 4 players and add the info for them and put them inside the arrayOfPlayers
+
+	// f - write function to iterate over the all players to decrease the level for the player if age larger the 30
+
+ //  g - sort the players in the arrayOfPlayers by the key
+	// 	sortPalyerBy(arrayOfPlayers, "age");
+	// 	sortPalyerBy(arrayOfPlayers, "name");
+ // */
 
 //writ your code here .....
+}
